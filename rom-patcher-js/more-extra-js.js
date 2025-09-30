ee36bb3c4c9ca7cbba9088dd863344a654359bea
PepGames <85426927+PepGames@users.noreply.github.com>
2025-09-30 08:38:50 -0400

    Merge DX 3.0.0 release (#1)
    * Initial 3.0 push

--- rom-patcher-js/more-extra-js.js
+++ rom-patcher-js/more-extra-js.js
@@ -1,10 +1,13 @@
-document.getElementById("versionSelect").innerHTML += "<option value='0'>testpatch (Latest)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='0'>DX 3.0.b (Beta)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='1'>DX 3.0.0 (Latest)</option>";
 
 
 document.getElementById("versionSelect").value = selectedVersion;
 
 function changeVersion() {
 	if (document.getElementById("versionSelect").value == 0) {
-		window.location.href = "./testpatch"
+		window.location.href = "./3.0.b"
+	} else if (document.getElementById("versionSelect").value == 1) {
+		window.location.href = "./3.0.0"
 	} 
 }
