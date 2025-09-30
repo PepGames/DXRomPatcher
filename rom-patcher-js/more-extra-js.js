cc3983abf2643dcc69ab48983c129ed3cd6db97f
PepGames <85426927+PepGames@users.noreply.github.com>
2025-09-30 06:24:26 -0400

    Initial 3.0 push

--- rom-patcher-js/more-extra-js.js
+++ rom-patcher-js/more-extra-js.js
@@ -1,10 +1,13 @@
-document.getElementById("versionSelect").innerHTML += "<option value='0'>testpatch (Latest)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='0'>DX 3.0.b (Beta)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='1'>DX 3.0.0 (Latest)</option>";
 
 
 document.getElementById("versionSelect").value = selectedVersion;
 
 function changeVersion() {
 	if (document.getElementById("versionSelect").value == 0) {
-		window.location.href = "./testpatch"
+		window.location.href = "./DX_3_0_b"
+	} else if (document.getElementById("versionSelect").value == 1) {
+		window.location.href = "./DX_3_0_0"
 	} 
 }
